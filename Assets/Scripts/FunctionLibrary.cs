ee0a1335d4f824852f26c2b19382f2b847b74898
Nicolò Taormina <nicktaormina3@gmail.com>
2022-08-19 14:31:44 +0200

    Torus twisting ring

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -69,13 +69,14 @@ public static class FunctionLibrary
 
     public static Vector3 Torus(float u, float v, float t)
     {
-        float r = 1f;
-        float s = 0.5f + r * Cos(0.5f * PI * v);
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
 
         Vector3 p;
 
         p.x = s * Sin(PI * u);
-        p.y = r * Sin(0.5f * PI * v);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
 
         return p;
